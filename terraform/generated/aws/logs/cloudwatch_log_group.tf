11eca43514f3d29b9d3646b3e8d84877524214e5
dodonki1223 <make_an_effort_wish_come_true@yahoo.co.jp>
2022-03-03 09:30:04 +0900

    :construction: IAM ロールの作成に CloudWatch Logs も必要だったため Terraformer でインポートする
    以下のコマンドを叩き、Lambda が自動で生成した CloudWatch Logs をインポートする。
    
    ```shell
    $ terraformer import aws --resources=logs --filter="Name=name;Value='/aws/lambda/translate-function'" --profile=terraform
    ```

---
+++ terraform/generated/aws/logs/cloudwatch_log_group.tf
@@ -0,0 +1,3 @@
+resource "aws_cloudwatch_log_group" "tfer---002F-aws-002F-lambda-002F-translate-function" {
+  name = "/aws/lambda/translate-function"
+}
---
+++ terraform/generated/aws/logs/outputs.tf
@@ -0,0 +1,3 @@
+output "aws_cloudwatch_log_group_tfer---002F-aws-002F-lambda-002F-translate-function_id" {
+  value = "${aws_cloudwatch_log_group.tfer---002F-aws-002F-lambda-002F-translate-function.id}"
+}
