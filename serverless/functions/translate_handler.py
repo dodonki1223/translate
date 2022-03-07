b3cdb4a38415d29df1a592fb1bdf2f3799174ea1
dodonki1223 <make_an_effort_wish_come_true@yahoo.co.jp>
2022-03-08 08:24:19 +0900

    :sparkles: クエリパラメータから取得した日本語を英語に変換する処理を追加
    API Gateway に日本語を受け取るクエリーパラメータ（input_text）を追加。
    Lambda で API Gateway から取得したクエリーパラメータを受け取り AWS Translate を使用して日本語を英語に変換し返す処理を追加。
    
    Lambda で使用する Python パッケージを requirements.txt にまとめて serverless-python-requirements プラグインでデプロイ時に自動で外部モジュールをインストールしてデプロイメントパッケージに同梱されるようにする。
    
    Ptyhon の外部モジュールは以下のコマンドを実行するとよい。
    
    ```shell
    $ sls requirements
    ```
    
    以下のコマンドを実行し requirements.txt を作成した。
    
    ```shell
    $ pip freeze > requirements.txt
    ```

--- serverless/functions/translate_handler.py
+++ serverless/functions/translate_handler.py
@@ -1,24 +1,23 @@
 import json
-
+import boto3
 
 def translate(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    input_text = event['queryStringParameters']['input_text']
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    translate = boto3.client('translate')
+    response = translate.translate_text(
+        Text=input_text,
+        SourceLanguageCode='ja',
+        TargetLanguageCode='en',
+    )
+    
+    output_text = response.get('TranslatedText')
 
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        'statusCode': 200,
+        'body': json.dumps({
+            'output_text': output_text
+        }),
+        'isBase64Encoded': False,
+        'headers': {}
     }
-    """
