dd4b2145dc953d3450533467559e6d0959360340
dodonki1223 <make_an_effort_wish_come_true@yahoo.co.jp>
2022-03-21 18:42:21 +0900

    :fire: Terraformer を使ってインポートした terraform のコードは必要なくなったので削除

--- terraform/generated/aws/logs/cloudwatch_log_group.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_cloudwatch_log_group" "tfer---002F-aws-002F-lambda-002F-translate-function" {
-  name = "/aws/lambda/translate-function"
-}
--- terraform/generated/aws/logs/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_cloudwatch_log_group_tfer---002F-aws-002F-lambda-002F-translate-function_id" {
-  value = "${aws_cloudwatch_log_group.tfer---002F-aws-002F-lambda-002F-translate-function.id}"
-}
